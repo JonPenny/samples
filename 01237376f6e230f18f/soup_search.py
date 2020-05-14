5061559dd3f46da7e0c475d400b8f68a620d833c
Jon Penny <jonpenny@Jons-MacBook-Pro.local>
2020-05-14 15:43:34 -0400

    Updated function to use internal article variable

--- 01237376f6e230f18f/soup_search.py
+++ 01237376f6e230f18f/soup_search.py
@@ -7,8 +7,8 @@ article.parse()
 
 def find_author(newspaper_article):
     # See if newspaper finds the articles authors
-    if article.authors and len(article) != 0:
-        return article.authors
+    if newspaper_article.authors and len(newspaper_article) != 0:
+        return newspaper_article.authors
     # If not attempt our own method
     else:
         search_term = "author"
@@ -16,18 +16,18 @@ def find_author(newspaper_article):
 
         # Step through all of the html picking out all of the classes
         while loc != -1:
-            new_loc = article.html[loc:].find("class=")
+            new_loc = newspaper_article.html[loc:].find("class=")
             if new_loc == -1:
                 break
 
             # Check if the class has the search term we want in it.
-            if article.html[loc:loc+70].find(search_term) != -1:
+            if newspaper_article.html[loc:loc+70].find(search_term) != -1:
 
                 # Remove the surrounding div
-                div_start = article.html[loc:loc+70].find(">") + 1
-                div_end = article.html[loc:loc+70].find("</div>")
+                div_start = newspaper_article.html[loc:loc+70].find(">") + 1
+                div_end = newspaper_article.html[loc:loc+70].find("</div>")
 
-                author_html_text = (article.html[loc+div_start:loc+div_end])
+                author_html_text = (newspaper_article.html[loc+div_start:loc+div_end])
 
                 # Remove any extra html elements
                 # I would improve this part as part of the project, elements should be removed by
